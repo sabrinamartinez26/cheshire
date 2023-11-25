18997ffea3faf37b6b31bcc69e54a1592ab11c91
sabrinamartinez <81254404+sabrinamartinez26@users.noreply.github.com>
2023-11-25 00:12:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -21,6 +21,8 @@ if($linhas == 0){
 <html>
 <head>
 	<title><?php echo $nome_sistema ?></title>
+	<link href="https://github.com/sabrinamartinez26/cheshire/css/style.css" rel="stylesheet">
+	<script src="https://github.com/sabrinamartinez26/cheshire/painel/js"> </script>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" type="image/x-icon" href="img/icone.png">
